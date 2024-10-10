5bd38cc1f8348bac7ae830bb5d4ca6e993d73f22
rakati <ouhaddounouredine@gmail.com>
2024-10-10 17:16:59 +0100

    ignore pycache files and add first dfs algorithm

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''A simple script that check if a graph is connected'''
+
+
+def canUnlockAll(boxes):
+    '''A function that check if boxes are unlocked
+    Parameters:
+    -----------
+        boxes: list
+            - list of list aka list of box on each box list of keys.
+            - box 0 is always unlocked
+    Return:
+    -------
+        True if all boxes can be unlocked otherwise False
+    '''
+    unlocked = set()
+
+    def dfs(v):
+        '''deep first search on all boxes to see if we can visit all of them'''
+        unlocked.add(v)
+        for box in boxes[v]:
+            if box not in unlocked:
+                dfs(box)
+
+    dfs(0)
+    return len(unlocked) == len(boxes)

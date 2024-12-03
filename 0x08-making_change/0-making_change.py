fcbda425f5b9aab957334d86c1433b6b9b9c2292
rakati <ouhaddounouredine@gmail.com>
2024-12-03 17:08:06 +0100

    find min number of coins to meet the total using dp

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -18,3 +18,24 @@ def makeChange(coins, total):
             - if total is 0 or less return 0
             - if total cannot be met by any number of coins return -1
     """
+    coins.sort()
+    dp = [[0] * (total + 1) for _ in range(len(coins))]
+
+    for i in range(len(coins) - 1, -1, -1):
+        for sub_total in range(1, total + 1):
+            dp[i][sub_total] = float("inf")
+            take = float("inf")
+            notake = float("inf")
+
+            # case of taking the current coin i
+            if sub_total - coins[i] >= 0:
+                take = dp[i][sub_total - coins[i]]
+                if take != float("inf"):
+                    take += 1
+            # case of not taking the current coin
+            if i + 1 < len(coins):
+                notake = dp[i + 1][sub_total]
+
+            dp[i][sub_total] = min(take, notake)
+
+    return dp[0][total] if dp[0][total] != float("inf") else -1
